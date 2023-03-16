c9dfc61e69fa752c0cb6a6865220f24739b467f3
Boardstroke <matheus.fachinis@gmail.com>
2023-03-16 10:56:49 -0300

    style(select.tsx): add dark mode support to select component and update hover styles

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -15,7 +15,7 @@ export const Select = ({ options, defaultValue }: SelectProps) => {
     const [isVisible, setIsVisible] = useState(false);
     return (
         <div>
-            <div className="flex gap-x-1 w-35 text-xs justify-between items-center px-4 py-2 cursor-pointer hover:bg-slate-100 rounded-full">
+            <div className="flex gap-x-1 w-35 text-xs justify-between items-center px-4 py-2 cursor-pointer hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-white  rounded-full">
                 <span>{defaultValue?.label}</span>
                 <SelectArrowDownIcon />
             </div>
