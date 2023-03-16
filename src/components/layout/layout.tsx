8d4030025cb42af2e15c45481d112cbd49ab82e7
Boardstroke <matheus.fachinis@gmail.com>
2023-03-16 10:56:10 -0300

    feat(layout.tsx): add dark mode support to layout component style(layout.tsx): adjust layout styles to accommodate dark mode

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,11 +1,18 @@
 import { PropsWithChildren } from "react";
+import { useTheme } from "../../contexts/theme";
 import { AppHeader } from "../app-header/app-header";
 
 export const Layout = ({ children }: PropsWithChildren) => {
+    const { theme } = useTheme();
+
     return (
-        <main>
-            <AppHeader />
-            <div className="max-w-2xl mx-auto mt-24 mb-12">{children}</div>
+        <main className={`${theme} `}>
+            <div className="flex flex-col dark:bg-slate-900 min-h-screen">
+                <AppHeader />
+                <div className="max-w-screen-md mx-auto flex-1 mt-24 dark:bg-slate-900">
+                    {children}
+                </div>
+            </div>
         </main>
     );
 };
