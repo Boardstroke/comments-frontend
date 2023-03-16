5a9f2a65c5f995bff8f7178494ec9331fc1a9052
Boardstroke <matheus.fachinis@gmail.com>
2023-03-16 10:55:37 -0300

    style(search-bar.tsx): adjust search bar styling and add dark mode support

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,19 +1,22 @@
 export const SearchBar = () => {
-  return (
-    <div className="relative w-full max-w-sm mx-auto">
-      <button className="absolute left-0 top-0 mt-3 mr-4">
-        <svg className="w-4 h-4 fill-current text-gray-500" viewBox="0 0 24 24">
-          <path
-            className="heroicon-ui"
-            d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
-          />
-        </svg>
-      </button>
-      <input
-        className="w-full pl-8 py-2 rounded-lg text-gray-700 focus:outline-none focus:shadow-outline"
-        type="text"
-        placeholder="Search..."
-      />
-    </div>
-  );
+    return (
+        <div className="relative w-full max-w-sm mx-auto ">
+            <button className="absolute left-4 top-0 mt-3 mr-4">
+                <svg
+                    className="w-4 h-4 fill-current text-gray-500"
+                    viewBox="0 0 24 24"
+                >
+                    <path
+                        className="heroicon-ui"
+                        d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
+                    />
+                </svg>
+            </button>
+            <input
+                className="w-full pl-10 py-2 rounded-lg text-gray-700 dark:bg-slate-900 dark:text-gray-200 focus:outline-none focus:shadow-outline"
+                type="text"
+                placeholder="Search..."
+            />
+        </div>
+    );
 };
