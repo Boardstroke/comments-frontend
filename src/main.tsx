5c98ffa2a7bf8d740e8ad741f4235a6fe5b94e23
Boardstroke <matheus.fachinis@gmail.com>
2023-03-16 10:58:42 -0300

    ✨ feat(main.tsx): add react-router-dom and create router for the app ✨ feat(main.tsx): add pages for 404, root, post, and posts ✨ feat(main.tsx): wrap the app with ThemeProvider and RouterProvider ✨ feat(pages/root.tsx): create Root component with Layout and Outlet components

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { CommentListProvider } from "./contexts/comment-list-context";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { CommentListProvider } from "./contexts/comment-list";
 import "./index.css";
+
+import { Page404 } from "./pages/404";
+import { Root } from "./pages/root";
+import { Post } from "./pages/post";
+import { ThemeProvider } from "./contexts/theme";
+import { Posts } from "./pages/posts";
 
 if (process.env.NODE_ENV === "development") {
     const { worker } = await import("./mock-server/msw-worker");
@@ -10,10 +17,30 @@ if (process.env.NODE_ENV === "development") {
     worker.start();
 }
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <Page404 />,
+        children: [
+            {
+                path: "/:userId/:postId",
+                element: <Post />,
+            },
+            {
+                path: "/",
+                element: <Posts />,
+            },
+        ],
+    },
+]);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <CommentListProvider>
-            <App />
-        </CommentListProvider>
+        <ThemeProvider>
+            <CommentListProvider>
+                <RouterProvider router={router} />
+            </CommentListProvider>
+        </ThemeProvider>
     </React.StrictMode>
 );
