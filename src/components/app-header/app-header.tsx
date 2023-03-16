b5003f787e1ef67098815a101c342f5b1ef98f92
Boardstroke <matheus.fachinis@gmail.com>
2023-03-16 10:55:45 -0300

    feat(app-header.tsx): add theme toggle functionality to the header

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,20 +1,22 @@
+import { Link } from "react-router-dom";
+import { useTheme } from "../../contexts/theme";
 import { Button } from "../button/button";
 import { SunIcon } from "../icons";
 import { MoonIcon } from "../icons/moon";
 import { UserIcon } from "../icons/user";
 import { SearchBar } from "../search-bar/search-bar";
-
 export const AppHeader = () => {
+    const { setTheme, theme } = useTheme();
     return (
-        <header className="bg-white py-4 px-10 shadow-md flex w-full justify-between fixed top-0">
+        <header className=" bg-white dark:bg-slate-900 border-b dark:border-b-slate-700 py-4 px-10 flex w-full justify-between fixed top-0">
             <div className="flex items-center relative ">
-                <h1 className="text-xl font-bold">
+                <Link to={"/"}>
                     <img
-                        src="mustache.png"
+                        src="/mustache.png"
                         alt="mustache logo"
                         className="w-12"
-                    />{" "}
-                </h1>
+                    />
+                </Link>
             </div>
 
             <div className="flex-1 justify-center items-center">
@@ -23,7 +25,16 @@ export const AppHeader = () => {
 
             <nav className="flex  relative items-end justify-end">
                 <Button.Group>
-                    <Button icon={<MoonIcon />} variant="text" rounded />
+                    <Button
+                        icon={theme == "light" ? <MoonIcon /> : <SunIcon />}
+                        variant="text"
+                        rounded
+                        onClick={() =>
+                            setTheme((prevTheme) =>
+                                prevTheme === "light" ? "dark" : "light"
+                            )
+                        }
+                    />
 
                     <Button icon={<UserIcon />} variant="text" rounded />
                 </Button.Group>
