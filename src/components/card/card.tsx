8f712659e744aadd3283c9ad37b1836e66f10982
Boardstroke <matheus.fachinis@gmail.com>
2023-04-13 11:55:28 -0300

    🆕 feat(card.tsx): add CardSize enum and CardProps type 🎨 style(card.tsx): add conditional hover styles to Card component based on isLink prop

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,8 +1,23 @@
 import { PropsWithChildren } from "react";
 
-export const Card = ({ children }: PropsWithChildren) => {
+
+export enum CardSize {
+    Small = "small",
+    Medium = "medium",
+    Large = "large",
+}
+
+export type CardProps = PropsWithChildren<{
+    isLink?: boolean;
+    size?: CardSize;
+}>;
+
+
+
+
+export const Card = ({ children, isLink = false }: CardProps) => {
     return (
-        <article className="bg-white dark:bg-slate-900 dark:border-slate-700 p-4 border-l-0 border-r-0 md:rounded-lg md:border-l md:border-r border border-gray-300 flex flex-col gap-y-4 hover:bg-gray-50 dark:hover:bg-slate-800 cursor-pointer">
+        <article className={`bg-white dark:bg-slate-900 dark:border-slate-700 p-4 border-l-0 border-r-0 md:rounded-lg md:border-l md:border-r border border-gray-300 flex flex-col gap-y-4 ${isLink && "hover:bg-gray-50 dark:hover:bg-slate-800 cursor-pointer"}`}>
             {children}
         </article>
     );
